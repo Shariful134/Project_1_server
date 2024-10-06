33a33a063c25693f574ac6f5abbb89d50fe3c6eb
Shariful islam <shariful32213@gmail.com>
2024-10-06 08:55:55 +0600

    token-verify

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-var jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 // midelware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gc7k6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,6 +29,47 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middelwares
+const logger = async (req, res, next) => {
+  console.log("called: ", req.host, req.originalUrl);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of token in the middelwares: ", token);
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).send({ message: "not authorized " });
+    }
+    // if token is valid then it would be decoded
+    console.log("value in the token: ", decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token;
+//   console.log("value of token in middelwares", token);
+//   if (!token) {
+//     return res.status(401).send({ message: "not authorized" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(401).send({ message: "not authorized" });
+//     }
+//     // if token is valid then it would be decoded
+//     console.log("value in the token", decoded);
+//     req.user = decoded;
+//     next();
+//   });
+// };
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,7 +78,22 @@ async function run() {
     const serviceCollection = client.db("carProject").collection("carService");
     const bookingCollection = client.db("carProject").collection("bookings");
 
-    app.get("/services", async (req, res) => {
+    // auth realeted api
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -48,18 +111,15 @@ async function run() {
     });
     //service releted web token
 
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
-      res.send(token);
-    });
-
     // Bookings
-    app.get("/bookings", async (req, res) => {
-      console.log(req.query.email);
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
+      console.log("query email: ", req.query.email);
+      console.log("User email: ", req.user.email);
+      console.log("toktoken", req.cookies.token);
+      console.log("user in the valid token: ", req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "forbidded authorized" });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
